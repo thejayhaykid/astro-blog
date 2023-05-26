7ea1c0dfff3132664c19409433914b915b199b9b
Jake Hayes <thejayhaykid@users.noreply.github.com>
2023-05-26 17:40:10 -0400

    Added some plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import netlify from "@astrojs/netlify/functions";
+import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
+
+const robotsSettings = {
+	sitemap: "https://www.jakehayes.net/sitemap.xml",
+}
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://www.jakehayes.net',
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(robotsSettings)],
+  output: "server",
+  adapter: netlify()
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = `Jake Hayes' Blog`;
+export const SITE_DESCRIPTION = `I'm a software engineer who loves to learn and share what I know.`;
