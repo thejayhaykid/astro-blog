f0288caa9e1a3c18bdca25e8b96011937b472390
Jake Hayes <thejayhaykid@users.noreply.github.com>
2023-05-27 15:33:46 -0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
 import netlify from "@astrojs/netlify/functions";
-import tailwind from "@astrojs/tailwind";
-
-import robotsTxt from "astro-robots-txt";
-
-const robotsSettings = {
-	sitemap: "https://www.jakehayes.net/sitemap.xml",
-}
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.jakehayes.net',
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(robotsSettings)],
+  site: "https://www.jakehayes.net",
+  integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
