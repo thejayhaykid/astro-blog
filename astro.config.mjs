bdf26297367599f9ce7d4d959239fe7de346890a
Jake Hayes <thejayhaykid@users.noreply.github.com>
2023-06-02 17:47:06 -0400

    Remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
-import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.jakehayes.net",
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
