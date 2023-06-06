366d03e25f5c192b58d95cfa3782e09ca39a7c6a
Jake Hayes <thejayhaykid@users.noreply.github.com>
2023-06-06 19:25:29 -0400

    Fix resume page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,5 +6,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [`@tailwindcss/forms`, "@tailwindcss/typography"],
+  plugins: [require(`@tailwindcss/forms`), require("@tailwindcss/typography")],
 };
