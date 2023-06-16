456e7f2734576eb8d6253c5d3c9c186f1bb4bd49
Jake Hayes <thejayhaykid@users.noreply.github.com>
2023-06-16 14:08:57 -0400

    trying to change the site to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/static";
 
 import tailwind from "@astrojs/tailwind";
 
