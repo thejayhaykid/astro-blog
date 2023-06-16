de71d20a6bb86262e0e9694ce8b1cd44511d5146
Jake Hayes <thejayhaykid@users.noreply.github.com>
2023-06-16 14:16:36 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify/static";
+import netlify from "@astrojs/netlify/functions";
 
 import tailwind from "@astrojs/tailwind";
 
