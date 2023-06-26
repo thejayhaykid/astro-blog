6aa36e669b30932a5c6221f5f35239a592337b28
Jake Hayes <thejayhaykid@users.noreply.github.com>
2023-06-26 10:43:52 -0400

    Switching to add some more react on the site for more interactivity

---
+++ src/components/Header.jsx
@@ -0,0 +1,131 @@
+import { Fragment, useEffect, useState } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars2Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const nav = [
+  { name: "Blog", href: "blog", current: false },
+  { name: "About", href: "about", current: false },
+  { name: "Resume", href: "resume", current: false },
+  { name: "Contact", href: "contact", current: false },
+  { name: "Links", href: "links", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Header() {
+  const [navigation, setNavigation] = useState(nav);
+  useEffect(() => {
+    const page = window.location.pathname.split("/")[1];
+
+    navigation.forEach((item) => {
+      if (item.href === page) {
+        setNavigation((prevNav) => {
+          return [
+            ...prevNav,
+            {
+              name: item.name,
+              href: item.href,
+              current: (item.current = true),
+            },
+          ];
+        });
+      } else {
+        setNavigation((prevNav) => {
+          return [
+            ...prevNav,
+            {
+              name: item.name,
+              href: item.href,
+              current: (item.current = false),
+            },
+          ];
+        });
+      }
+    });
+
+    console.log(navigation);
+  }, []);
+
+  return (
+    <Disclosure
+      as="nav"
+      className="dark:bg-gray-800 border-b-2 border-gray-800"
+    >
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black dark:focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars2Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="block h-8 w-auto lg:hidden"
+                    src="/JH.png"
+                    alt="Jake Hayes"
+                  />
+                  <img
+                    className="hidden h-8 w-auto lg:block"
+                    src="/JH.png"
+                    alt="Jake Hayes"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-200 text-black dark:bg-gray-900 dark:text-white"
+                            : "text-gray-900 hover:bg-gray-100 hover:text-black dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white",
+                          "rounded-md px-3 py-2 text-sm font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-200 text-black dark:bg-gray-900 dark:text-white"
+                      : "text-gray-900 hover:bg-gray-100 hover:text-black dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
