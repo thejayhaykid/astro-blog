f0107d456171f33f87e0f34fb889528fa3029c66
Jake Hayes <thejayhaykid@users.noreply.github.com>
2024-02-22 17:15:50 -0500

    Fix warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
