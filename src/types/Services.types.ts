5dcb28a658ca17f4f479b3ccbc462a549346c469
Jake Hayes <thejayhaykid@users.noreply.github.com>
2024-03-30 18:49:07 -0400

    WIP

---
+++ src/types/Services.types.ts
@@ -0,0 +1,9 @@
+export type Service = {
+  title: string;
+  description: string;
+  price: number;
+  frequency: string;
+  features: string[];
+  url: string;
+  primary: boolean;
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,5 +12,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require(`@tailwindcss/forms`), require("@tailwindcss/typography")],
+  plugins: [
+    require(`@tailwindcss/forms`),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
 };
