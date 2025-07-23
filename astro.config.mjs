76abb7e700e888f1f76fc4cbbad0a549350df9dc
Jake Hayes <thejayhaykid@users.noreply.github.com>
2025-07-23 12:21:01 -0400

    Trying this (#35)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,23 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.jakehayes.net",
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [mdx(), sitemap(), tailwind(), react(), partytown()],
   output: "server",
-  adapter: netlify(),
+  adapter: netlify({
+    redirects: [
+      {
+        from: "/stats.js",
+        to: "https://cloud.umami.is/script.js",
+        status: 200,
+      },
+    ],
+  }),
   prefetch: true,
 });
